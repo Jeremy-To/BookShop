bc53e86c8452510e023615dab2ff81d6fa29d0bb
jeremy-to <jeremy_to@outlook.fr>
2023-01-31 14:25:00 +0100

    fixing background image and adding gitignore

--- shopWebsite/src/components/items/HomePage.jsx
+++ shopWebsite/src/components/items/HomePage.jsx
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 
 function HomePage() {
 	return (
-		<div>
+		<div className={classes.section}>
 			<h1 className={classes.title}>Welcome to my shop !</h1>
 			<div className={classes.image}>
 				<div className={classes.link}>
                     <h2>The place to sell your books</h2>
 					<Link to="/shop">Go to store</Link>
 				</div>
-				<img src="./public/LibraryShop.jpg" alt="Image of a library shop" />
+				<img src="./public/Library.jpg" alt="Image of a library shop" />
 			</div>
 		</div>
 	);
