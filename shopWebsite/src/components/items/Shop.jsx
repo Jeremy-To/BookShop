a4a898e6711c4e54702dfeee5ead433d7c0ddded
jeremy-to <jeremy_to@outlook.fr>
2023-01-31 09:31:48 +0100

    $ added

--- shopWebsite/src/components/items/Shop.jsx
+++ shopWebsite/src/components/items/Shop.jsx
@@ -31,7 +31,7 @@ function Shop(props) {
 				</div>
 				<div className={classes.content}>
 					<h3>{props.title}</h3>
-					<p>{props.price}</p>
+					<p>{props.price} $</p>
 					<p>{props.description}</p>
 				</div>
 				<div className={classes.actions}>
