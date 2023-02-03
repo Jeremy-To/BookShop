c87cd7de278e25b095f3e2f7df136bbb42a06645
jeremy-to <jeremy_to@outlook.fr>
2023-02-03 15:35:49 +0100

    tailwind implementation (not finished)

--- shopWebsite/src/components/items/HomePage.jsx
+++ shopWebsite/src/components/items/HomePage.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import classes from './HomePage.module.css';
 import { Link } from 'react-router-dom';
 
 function HomePage() {
 	return (
-		<div className={classes.section}>
-			<h1 className={classes.title}>Welcome to my shop !</h1>
-			<div className={classes.image}>
-				<div className={classes.link}>
-                    <h2>The place to sell your books</h2>
-					<Link to="/shop">Go to store</Link>
+		<div>
+			<h1 className="font-bold text-4xl flex justify-center items-center">Welcome to my shop !</h1>
+			<div className="flex flex-wrap relative justify-center items-center">
+				<div className="flex flex-col justify-center items-center p-10 absolute rounded-md bg-white">
+					<h2 className='text-xl'>The place to sell your books</h2>
+					<Link to="/shop" className='hover:text-blue-600'>Go to store</Link>
 				</div>
-				<img src="/Library.jpg" alt="Image of a library shop" />
+				<img src="/Library.jpg" alt="Image of a library shop"/>
 			</div>
 		</div>
 	);
