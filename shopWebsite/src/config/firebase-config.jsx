445bcafa2b1f74bd656e360788aece3e033e26be
jeremy-to <jeremy_to@outlook.fr>
2023-02-14 15:07:00 +0100

    apikey changes

--- shopWebsite/src/config/firebase-config.jsx
+++ shopWebsite/src/config/firebase-config.jsx
@@ -1,15 +1,15 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyArOKOeEF8jHdbCi4BSfbu4vUuJZB9kh78",
-  authDomain: "fir-bookshop-5318e.firebaseapp.com",
-  projectId: "fir-bookshop-5318e",
-  storageBucket: "fir-bookshop-5318e.appspot.com",
-  messagingSenderId: "532550745378",
-  appId: "1:532550745378:web:724aa25d97fc8e75eeb97b",
+	apiKey: 'AIzaSyBVbU5j-BCm9UfMztACGpynFDLBWammYGA',
+	authDomain: 'chat-1a09d.firebaseapp.com',
+	projectId: 'chat-1a09d',
+	storageBucket: 'chat-1a09d.appspot.com',
+	messagingSenderId: '1048020367320',
+	appId: '1:1048020367320:web:baf7ebd39faf426c7fa4ad',
 };
 
 const app = initializeApp(firebaseConfig);
